479ce94c06fb94496808dabc79193bd5bd820f45
David Tang <dtang859@gmail.com>
2019-01-29 22:45:48 -0800

    adds student pics

---
+++ js/index.js
@@ -0,0 +1,30 @@
+(function(w) {
+  let { course, year } = w.DAVID;
+  let url = 'https://davids-students.herokuapp.com';
+
+  fetch(`${url}/api/${year}/${course}/students`)
+    .then((response) => {
+      return response.json();
+    })
+    .then(buildHTML)
+    .then(insertIntoPage);
+
+  function buildHTML({ students }) {
+    return students
+      .map(({ name, image }) => {
+        let [last, first, middle] = name.split(', ');
+
+        return `
+          <div class="student">
+            <img src="${url}${image}" alt="${first} image">
+            <small>${first}</small>
+          </div>
+        `;
+      })
+      .join('');
+  }
+
+  function insertIntoPage(html) {
+    document.querySelector('#students-container').innerHTML = html;
+  }
+})(window);
