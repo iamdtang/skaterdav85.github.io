b3ff74dd719462d8b455aa1f5bccbbfa36c32975
David Tang <dtang@auditboard.com>
2020-10-25 19:49:30 -0700

    Removes list of students in sidebar

--- svelte-components/src/main.js
+++ svelte-components/src/main.js
@@ -1,17 +1,17 @@
 import StudentList from './StudentList.svelte';
 import AutocompleteContainer from './AutocompleteContainer.svelte';
 
-if (window.DAVID) {
-  const { course, year } = window.DAVID;
+// if (window.DAVID) {
+//   const { course, year } = window.DAVID;
 
-  new StudentList({
-    target: document.querySelector('#students-container'),
-    props: {
-      course,
-      year
-    }
-  });
-}
+//   new StudentList({
+//     target: document.querySelector('#students-container'),
+//     props: {
+//       course,
+//       year
+//     }
+//   });
+// }
 
 const autocompleteContainer = document.querySelector('#autocomplete-container');
 
